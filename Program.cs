6a7ed48ac0d956e4fa173cfe50be118e6d9a9c8e
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-04-01 05:51:04 -0300

    exercicio terminado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿decimal valorpago, valorproduto, troco;
+
+Console.WriteLine("Qual o valor do produto? ");
+valorproduto = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Qual o valor pago? (em R$) ");
+valorpago = Convert.ToDecimal(Console.ReadLine()); 
+
+troco = (valorpago - valorproduto);
+
+Console.WriteLine($"O valor do troco é {troco}");
